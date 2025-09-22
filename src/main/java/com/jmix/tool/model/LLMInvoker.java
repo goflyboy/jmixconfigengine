961d2f26a81f87830f8e9407c75e22eb264d321e
超级丹 <21713279@qq.com>
2025-09-22 22:53:08 +0800

    修复instancof

--- src/main/java/com/jmix/tool/model/LLMInvoker.java
+++ src/main/java/com/jmix/tool/model/LLMInvoker.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Properties;
 
 import dev.langchain4j.data.message.AiMessage;
