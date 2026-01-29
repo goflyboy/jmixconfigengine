68a7d45236a742bbc42899097976088f7d53c0bc
超级丹 <21713279@qq.com>
2026-01-30 06:58:36 +0800

    添加SolverResult

---
+++ src/main/java/com/jmix/executor/cmodel/SolverResult.java
@@ -0,0 +1,56 @@
+package com.jmix.executor.cmodel;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 解决方案结果类
+ */
+@Data
+public class SolverResult {
+
+    private List<ModuleInst> solutions = new ArrayList<>();
+
+    private String solverStatus;
+
+    private boolean hasSearchMax = false;// 是否达到搜索的最大值
+
+    private int searchMax = -1;// -1,表示没有设置，搜索的最大值
+
+    private int iterationTimes = 0;// 迭代次数
+
+    public SolverResult() {
+    }
+
+    public List<ModuleInst> getSolutions() {
+        return solutions;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("iterationTimes:").append(iterationTimes).append(" searchMax:").append(searchMax)
+                .append(" hasSearchMax:").append(hasSearchMax).append("\n");
+        sb.append("solutions size:").append(solutions.size()).append("\n");
+        for (int i = 0; i < solutions.size(); i++) {
+            sb.append("Solu").append(i + 1).append(": ");
+            sb.append(solutions.get(i).toString()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String toShortString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("iterationTimes:").append(iterationTimes).append(" searchMax:").append(searchMax)
+                .append(" hasSearchMax:").append(hasSearchMax).append("\n");
+        sb.append("solverStatus:").append(solverStatus).append("\n");
+        sb.append("solutions size:").append(solutions.size()).append("\n");
+        for (int i = 0; i < solutions.size(); i++) {
+            sb.append("Solu").append(i + 1).append(": ");
+            sb.append(solutions.get(i).toShortString(true)).append("\n");
+        }
+        return sb.toString();
+    }
+}
